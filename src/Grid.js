13e124ea58bed0fc1d9fe20cbccb9aaedd97bd6e
Marcos Paulo <marcos_paulo.rodrigues@hotmail.com>
2022-07-19 18:06:03 -0300

    Inserindo a classe Grid.js

---
+++ src/Grid.js
@@ -0,0 +1,43 @@
+class Grid{
+    constructor(g){
+        this.grid = g;
+        this.sF = this.scaleFactor();
+    }
+
+    scaleFactor(){
+        return width / this.grid;
+    }
+
+    drawGrid(){
+        for (var i = 0; i < this.grid * this.sF; i += this.sF) {
+            // linhas de Y
+            line(i, 1, i, height);
+            // linhas de X
+            line(1, i, width, i);
+    
+            // linhas de -Y
+            line(-i,1,-i,-height);
+            // // linhas de -X
+            line(1,-i,-width,-i);
+    
+            // // linhas de Y
+            line(-i,1,-i,height);
+            // // linhas de -x
+            line(1,i,-width,i);
+    
+            // // linhas de -Y
+            line(i,1,i,-height);
+            // // linhas de X
+            line(1,-i,width,-i);
+        }
+        // Determina as linhas do plano cartesiano
+        stroke(0);
+        strokeWeight(4);
+        line(0,0,0,height);
+        line(0,0,width,0);
+        line(0,0,0,-height);
+        line(0,0,-width,0);
+
+        return;
+    }
+}
